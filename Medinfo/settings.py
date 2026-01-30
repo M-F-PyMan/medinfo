225210c4b2472b2e40587861d231d97c22316a30
M_F_Pyman <smkfm1376@gmail.com>
2026-01-31 01:24:01 +0330

    accounts changes

--- Medinfo/settings.py
+++ Medinfo/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #our apps
+    'accounts.apps.AccountsConfig',
+    'payments.apps.PaymentsConfig',
+    'courses.apps.CoursesConfig',
 ]
 
 MIDDLEWARE = [
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.utils import timezone
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        if not username:
+            raise ValueError('The Username field must be set')
+
+        email = self.normalize_email(email) if email else None
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(username, email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    username = models.CharField(max_length=100, unique=True,null=True,blank=True)
+    is_staff = models.BooleanField(default=False)
+
+    # فیلدهای ضروری برای authentication system
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []  # فیلدهای مورد نیاز برای createsuperuser
+
+    def __str__(self):
+        return self.name or self.username
+
+    def get_full_name(self):
+        return self.name or self.username
+
+    def get_short_name(self):
+        return self.username
+
