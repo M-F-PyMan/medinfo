0b086a4c14a7831b5e966c89698dcb6a3e59e9b8
M_F_Pyman <smkfm1376@gmail.com>
2026-02-15 14:59:13 +0330

    new changes

--- courses/views.py
+++ courses/views.py
@@ -7,6 +7,36 @@ from .serializers import (
     LessonSerializer,
 )
 
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from accounts.models import Enrollment
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all().order_by("-id")
+    lookup_field = "slug"
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return CourseListSerializer
+        return CourseDetailSerializer
+
+    # -------------------------
+    #  بررسی دسترسی کاربر
+    # -------------------------
+    @action(detail=True, methods=["get"], permission_classes=[IsAuthenticated])
+    def has_access(self, request, slug=None):
+        course = self.get_object()
+        user = request.user
+
+        enrolled = Enrollment.objects.filter(user=user, course=course).exists()
+
+        return Response({
+            "enrolled": enrolled,
+            "course_id": course.id,
+            "user_id": user.id
+        })
+
 
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.all().order_by("-id")
